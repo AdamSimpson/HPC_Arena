25a6f32b82a38dc7e41bd94e7e03d9664f0d976d
AdamSimpson <adambsimpson@gmail.com>
2016-08-21 13:49:09 -0400

    Update travis build

--- test.sh
+++ test.sh
@@ -7,10 +7,11 @@ mkdir -p $ROOT/vendor/SFML/build; cd $ROOT/vendor/SFML/build
 mkdir -p $ROOT/vendor/install
 cmake -DCMAKE_INSTALL_PREFIX:PATH=$ROOT/vendor/install $ROOT/vendor/SFML
 make
+make install
 ls -RAhlF $ROOT/vendor/install
 
 cd $ROOT
 
 mkdir -p $ROOT/build; cd $ROOT/build
-CXX=g++-6 cmake -DSFML_ROOT=$ROOT/vendor/install/SFML $ROOT
+CXX=g++-6 cmake -DSFML_ROOT=$ROOT/vendor/install $ROOT
 make VERBOSE=1
